9c5915124cc71efc4e075f4ac32eaeead57bb356
Willy De Keyser <spring.boot.programming@gmail.com>
2026-03-27 11:54:02 +0100

    Init

---
+++ Spring-Boot-Security-Thymeleaf-Custom-One-Time-Token-Extra-Fields-part21/src/main/java/willydekeyser/SpringBootSecurityThymeleafCustomOneTimeTokenExtraFieldsPart21Application.java
@@ -0,0 +1,13 @@
+package willydekeyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityThymeleafCustomOneTimeTokenExtraFieldsPart21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityThymeleafCustomOneTimeTokenExtraFieldsPart21Application.class, args);
+	}
+
+}
---
+++ Spring-Boot-Security-Thymeleaf-Custom-One-Time-Token-Extra-Fields-part21/src/test/java/willydekeyser/SpringBootSecurityThymeleafCustomOneTimeTokenExtraFieldsPart21ApplicationTests.java
@@ -0,0 +1,13 @@
+package willydekeyser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityThymeleafCustomOneTimeTokenExtraFieldsPart21ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
